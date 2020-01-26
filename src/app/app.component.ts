305ddfbdfc0c11afa7bd7d6a8cb3c9fa3fff6c7e
Brian Canedo <brian.canedo.600@my.csun.edu>
2020-01-25 17:53:37 -0800

    Used date pipe to formate user inputted date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
-  date: string;
+  dateRaw: string;
 
   onNameChange(value: string) {
     this.name = value;
   }
 
   onDateChange(value: string) {
-    this.date = value;
+    this.dateRaw = value;
   }
 }
